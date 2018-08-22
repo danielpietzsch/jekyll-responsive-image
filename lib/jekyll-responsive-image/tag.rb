651c2d6336add5ea3fd1806db7f95972a1682cd7
Daniel Pietzsch <me@danielpietzsch.com>
2018-08-22 22:35:19 +0200

    basic prototype for converting parameters from markdown
    in this case, I wanted to use markdown syntax in the caption I pass to the responsive_image tag

--- lib/jekyll-responsive-image/tag.rb
+++ lib/jekyll-responsive-image/tag.rb
@@ -13,7 +13,21 @@ module Jekyll
       end
 
       def render(context)
+        @attributes['caption'] = markdownify(context, @attributes['caption'])
+
         Renderer.new(context.registers[:site], @attributes).render_responsive_image
+      end
+
+      private
+
+      def markdownify(context, markdown_string)
+        html_string = context.registers[:site].find_converter_instance(::Jekyll::Converters::Markdown).convert(markdown_string)
+
+        remove_paragraph_tag_wrapping(html_string)
+      end
+
+      def remove_paragraph_tag_wrapping(html_string)
+        Regexp.new('^<p>(.*)<\/p>$').match(html_string)[1]
       end
     end
   end
